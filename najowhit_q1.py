d1648b53a37ed6a8aac9744c6d37368014ec0dce
Nathan White <najowhit@iupui.edu>
2017-04-26 23:38:38 -0400

    abstracted a function

--- najowhit_q1.py
+++ najowhit_q1.py
@@ -1,3 +1,6 @@
+"""
+CREATED BY: NATHAN WHITE
+"""
 import numpy
 import pandas
 from sklearn import svm
@@ -28,12 +31,39 @@ labels4 = labels[327:436]
 
 #test = numpy.concatenate((fold1, fold2, fold3, fold4))
 
-
-
 # Hypothesis Testing
 linear_svm = svm.SVC(C=1, kernel='linear')
 linear_svm.fit(fold4, labels4)
 linear_svm.predict(fold4)
 linear_svm.score(fold4, labels4)
 
-# What about the standard deviation?
+
+def svm_classify(training1, training2, training_labels1, training_labels2, test_data, test_labels):
+
+    combined_train = numpy.concatenate((training1, training2))
+    combined_labels = numpy.concatenate((training_labels1, training_labels2))
+
+    linear_svm.fit(combined_train, combined_labels)
+    linear_svm.predict(test_data)
+    accuracy = linear_svm.score(test_data, test_labels)
+    return accuracy
+
+
+
+# Fold1 as test, Fold2 & Fold3 as training
+fold23 = numpy.concatenate((fold2, fold3))
+labels23 = numpy.concatenate((labels2, labels3))
+linear_svm.fit(fold23, labels23)
+
+linear_svm.predict(fold1)
+print(linear_svm.score(fold1, labels1))
+print(svm_classify(fold2, fold3, labels2, labels3, fold1, labels1))
+
+# Fold2 as test, Fold1 & Fold3 as training
+fold13 = numpy.concatenate((fold1, fold3))
+labels13 = numpy.concatenate((fold1, fold3))
+
+linear_svm.predict(fold2)
+
+
+
