055b23f17827947fe408ca55d6b457cdabc3791d
Nathan White <najowhit@iupui.edu>
2017-04-27 13:49:39 -0400

    part one done

--- najowhit_q1.py
+++ najowhit_q1.py
@@ -65,7 +65,41 @@ accuracy_list.append(fold3_accuracy)
 average_accuracy = sum(accuracy_list)/3
 std = numpy.std(accuracy_list)
 
-print('Average Accuracy: ' + str(average_accuracy))
-print('Standard Deviation: ' + str(std))
+print('Linear Avg Acc: ' + str(average_accuracy))
+print('Linear Standard Dev: ' + str(std))
+
+# RBF Hypothesis Test
+rbf_svm = svm.SVC(C=3, kernel='rbf')
+rbf_svm.fit(fold4, labels4)
+rbf_svm.predict(fold4)
+rbf_svm.score(fold4, labels4)
 
 
+def rbf_svm_classify(training1, training2, training_labels1, training_labels2, test_data, test_labels):
+
+    combined_train = numpy.concatenate((training1, training2))
+    combined_labels = numpy.concatenate((training_labels1, training_labels2))
+
+    rbf_svm.fit(combined_train, combined_labels)
+    rbf_svm.predict(test_data)
+    accuracy = rbf_svm.score(test_data, test_labels)
+    return accuracy
+
+rbf_accuracy_list = []
+# Fold1 as test, Fold2 & Fold3 as training
+fold1_accuracy = rbf_svm_classify(fold2, fold3, labels2, labels3, fold1, labels1)
+rbf_accuracy_list.append(fold1_accuracy)
+
+# Fold2 as test, Fold1 & Fold3 as training
+fold2_accuracy = rbf_svm_classify(fold1, fold3, labels1, labels3, fold2, labels2)
+rbf_accuracy_list.append(fold2_accuracy)
+
+# Fold3 as test, Fold1 & Fold2 as training
+fold3_accuracy = svm_classify(fold1, fold2, labels1, labels2, fold3, labels3)
+rbf_accuracy_list.append(fold3_accuracy)
+
+rbf_average_accuracy = sum(rbf_accuracy_list)/3
+rbf_std = numpy.std(rbf_accuracy_list)
+
+print('RBF Avg Acc: ' + str(rbf_average_accuracy))
+print('RBF Standard Dev: ' + str(rbf_std))
